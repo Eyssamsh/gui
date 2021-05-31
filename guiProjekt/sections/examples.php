cd779eccbbd9c5b1f8849ea65d693b339ae9e9b3
Leonetienne <leonetienne@hotmail.de>
2021-05-31 17:10:32 +0200

    Mehrsprachigfied section/examples

--- guiProjekt/sections/examples.php
+++ guiProjekt/sections/examples.php
@@ -1,32 +1,38 @@
+
+<?php
+    // Shortcut
+    $local_sctnExmp = $local["body"]["examples"];
+?>
+
 <section class="features section-padding" data-scroll-index="2" id="beispiele">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8"></div>
             <div class="section-title">
-                <h2> Einfache <span> Beispiele</span> </h2>
+                <h2><?php echo($local_sctnExmp["title"]); ?></h2>
             </div>
         </div>
         <div class="row">
             <div class="owl-carousel features-carousel">
                 <div class="feature-item">
                     <div class="icon"> <i class="fas fa-code"></i> </div>
-                    <h3>Android App</h3>
-                    <p>Wir haben für euch ein kleines einfaches komplettes Android App , die könnt ihr herunterladen und das Porjekt einfach offnen und weiter arbeiten .</p>
+                    <h3><?php echo($local_sctnExmp["featureItems"]["android"]["title"]); ?></h3>
+                    <p><?php echo($local_sctnExmp["featureItems"]["android"]["description"]); ?></p>
                 </div>
                 <div class="feature-item">
                     <div class="icon"> <i class="fas fa-code"></i> </div>
-                    <h3>Php - statisch -</h3>
-                    <p>Wir haben auch für euch ein einfaches Beispiel in der Php Sprache ,damit man in der Mehrsprachigkeit einsteigen kann. Was hier mit statisch und dynamisch gemeient ist , könnt gerne in unsere Ausarbeitung nachschauen.</p>
+                    <h3><?php echo($local_sctnExmp["featureItems"]["php_stat"]["title"]); ?></h3>
+                    <p><?php echo($local_sctnExmp["featureItems"]["php_stat"]["description"]); ?></p>
                 </div>
                 <div class="feature-item">
                     <div class="icon"> <i class="fas fa-code"></i> </div>
-                    <h3>Php - Dynamisch - </h3>
-                    <p>Wir haben auch für euch ein einfaches Beispiel in der Php Sprache ,damit man in der Mehrsprachigkeit einsteigen kann.</p>
+                    <h3><?php echo($local_sctnExmp["featureItems"]["php_dyn"]["title"]); ?></h3>
+                    <p><?php echo($local_sctnExmp["featureItems"]["php_dyn"]["description"]); ?></p>
                 </div>
                 <div class="feature-item">
                     <div class="icon"> <i class="fas fa-check"></i> </div>
-                    <h3>Javascript - statisch - </h3>
-                    <p> Diese Webseite wurde mit Javascript entwickelt , die wurden wir auch zur Verfügung stellen.</p>
+                    <h3><?php echo($local_sctnExmp["featureItems"]["js_stat"]["title"]); ?></h3>
+                    <p><?php echo($local_sctnExmp["featureItems"]["js_stat"]["description"]); ?></p>
                 </div>
             </div>
         </div>
