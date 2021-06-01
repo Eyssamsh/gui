688b373e7f6023628fd88dd6f1972a3a21655a58
Leonetienne <leonetienne@hotmail.de>
2021-06-01 22:46:21 +0200

    Fixed issue with fallback language, if no get-param is provided

--- guiProjekt/lang/loadLang.php
+++ guiProjekt/lang/loadLang.php
@@ -1,10 +1,18 @@
 <?php
-    $requestedLang = $_GET["lang"];
-    $local = file_get_contents("./lang/loc/" . $requestedLang . ".json");
 
-    // Fallback language
+    if (isset($_GET["lang"]))
+    {
+        $requestedLang = $_GET["lang"];
+        $local = file_get_contents("./lang/loc/" . $requestedLang . ".json");
+    }
+    else
+    {
+        $local = NULL;
+    }
+    
+    // Load fallback language
     if (!$local)
-        $local = file_get_contents("./lang/dede.json");
+        $local = file_get_contents("./lang/loc/dede.json");
 
     $local = json_decode($local, true);
 ?>
